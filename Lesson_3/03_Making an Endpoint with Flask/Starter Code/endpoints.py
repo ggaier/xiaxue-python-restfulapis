d036d7a4a5a8546eec6ddff657798d6a97334153
ggaier <jwenbo52@gmail.com>
2018-08-05 23:14:36 +0800

    add flask route

--- Lesson_3/03_Making an Endpoint with Flask/Starter Code/endpoints.py
+++ Lesson_3/03_Making an Endpoint with Flask/Starter Code/endpoints.py
@@ -4,12 +4,14 @@ app = Flask(__name__)
 
 #Make an app.route() decorator here for when the client sends the URI "/puppies"
 
+@app.route('/puppies')
 def puppiesFunction():
   return "Yes, puppies!"
   
  
 #Make another app.route() decorator here that takes in an integer named 'id' for when the client visits a URI like "/puppies/5"
 
+@app.route('/puppies/<int:id>')
 def puppiesFunctionId(id):
   return "This method will act on the puppy with id %s" % id
 
