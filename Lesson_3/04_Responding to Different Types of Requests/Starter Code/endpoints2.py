363f6a3f0c7ef2f2ff0c61ed79e4522d42c7bf86
ggaier <jwenbo52@gmail.com>
2018-08-06 22:00:25 +0800

    finish respond to different kinds of request

--- Lesson_3/04_Responding to Different Types of Requests/Starter Code/endpoints2.py
+++ Lesson_3/04_Responding to Different Types of Requests/Starter Code/endpoints2.py
@@ -1,31 +1,33 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 # Create the appropriate app.route functions, test and see if they work, and paste your URIs in the boxes below.
 
 #Make an app.route() decorator here
 
+
+@app.route('/puppies', methods = ['GET', 'POST'])
 def puppiesFunction():
   if request.method == 'GET':
   	#Call the method to Get all of the puppies
-  	
-  
+  	return getAllPuppies()
   elif request.method == 'POST':
   	#Call the method to make a new puppy
-  
+    return makeANewPuppy()
   
  
 #Make another app.route() decorator here that takes in an integer id in the 
-
+@app.route('/puppies/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def puppiesFunctionId(id):
   if request.method == 'GET':
   	#Call the method to get a specific puppy based on their id
-  	
+  	return getPuppy(id)
   if request.method == 'PUT':
   	#Call the method to update a puppy
-  	
+  	return updatePuppy(id)
   elif request.method == 'DELETE':
   	#Call the method to remove a puppy
-  	
+  	return deletePuppy(id)
 
 
 def getAllPuppies():
@@ -42,3 +44,7 @@ def updatePuppy(id):
 
 def deletePuppy(id):
   return "Removing Puppy with id %s" % id
+
+if __name__ =='__main__':
+  app.debug = True
+  app.run(host='0.0.0.0', port=5000)
