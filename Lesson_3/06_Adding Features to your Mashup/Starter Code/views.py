c6a23dedefe652abd6261f422d9717e1ead23c5e
ggaier <jwenbo52@gmail.com>
2018-08-08 23:29:27 +0800

    finish adding feature to your mashup

--- Lesson_3/06_Adding Features to your Mashup/Starter Code/views.py
+++ Lesson_3/06_Adding Features to your Mashup/Starter Code/views.py
@@ -34,12 +34,41 @@ def all_restaurants_handler():
     restaurants = session.query(Restaurant).all()
     return jsonify(restaurants=[i.serialize for i in restaurants])
   elif request.method == 'POST':
-    location = request.args.get('location', "")
+    location = request.args.get('location', '')
+    meal_type = request.args.get('mealType', '')
+    restaurant_info = findARestaurant(meal_type, location)
+    if restaurant_info != 'No Restaurants Found':
+      restaurant = Restaurant(restaurant_name=unicode(restaurant_info['name']),
+                              restaurant_address=unicode(restaurant_info['address']),
+                              restaurant_image=restaurant_info['image'])
+      session.add(restaurant)
+      session.commit()
+      return jsonify(restaurant = restaurant.serialize)
+    else:
+      return jsonify({"error": "No Restaurants Found for %s in %s" % (mealType, location)})
     
 @app.route('/restaurants/<int:id>', methods = ['GET','PUT', 'DELETE'])
 def restaurant_handler(id):
   #YOUR CODE HERE
-  
+  restaurant = session.query(Restaurant).filter_by(id=id).one()
+  if request.method == 'GET':
+    return jsonify(restaurant = restaurant.serialize)
+  elif request.method == 'PUT':
+    address = request.args.get('address')
+    image = request.args.get('image')
+    name = request.args.get('name')
+    if address:
+      restaurant.restaurant_address = address
+    if image: 
+      restaurant.restaurant_image= image
+    if name: 
+      restaurant.restaurant_name = name
+    session.commit()
+    return jsonify(restaurant = restaurant.serialize)
+  elif request.method == 'DELETE':
+    session.delete(restaurant)
+    session.commit()
+    return 'Restaurant deleted'
 
 if __name__ == '__main__':
     app.debug = True
