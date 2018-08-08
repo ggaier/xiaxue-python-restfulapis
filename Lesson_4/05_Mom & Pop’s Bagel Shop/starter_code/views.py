ea5267c2f769906de265ac4f585b24a0cb3bf6b8
ggaier <jwenbo52@gmail.com>
2018-08-08 23:58:28 +0800

    add verify password method

--- Lesson_4/05_Mom & Pop’s Bagel Shop/starter_code/views.py
+++ Lesson_4/05_Mom & Pop’s Bagel Shop/starter_code/views.py
@@ -16,13 +16,36 @@ session = DBSession()
 app = Flask(__name__)
 
 #ADD @auth.verify_password here
+@auth.verify_password
+def verify_password(username, password):
+    user = session.query(User).filter_by(username = username).first()
+    if not user:
+        return False
+    elif not user.verify_password(password):
+        return False
+    else:
+        g.user = user
+        return True
 
 #ADD a /users route here
-
-
+@app.route('/users', methods = ['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)
+    if session.query(User).filter_by(username = username).first() is not None:
+        return jsonify({'message':'user already exists'}), 200
+    
+    user = User(username = username)
+    user.hash_password(password)
+    session.add(user)
+    session.commit()
+    return jsonify({'username':user.username}), 201
 
 @app.route('/bagels', methods = ['GET','POST'])
 #protect this route with a required login
+@auth.login_required
 def showAllBagels():
     if request.method == 'GET':
         bagels = session.query(Bagel).all()
