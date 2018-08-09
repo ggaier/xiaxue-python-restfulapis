e189e4e613f0350e7ebd583e69e8a4e93fb151a5
ggaier <jwenbo52@gmail.com>
2018-08-10 00:04:11 +0800

    finish mom&pop bagel shop

--- Lesson_4/05_Mom & Pop’s Bagel Shop/starter_code/models.py
+++ Lesson_4/05_Mom & Pop’s Bagel Shop/starter_code/models.py
@@ -6,6 +6,18 @@ from passlib.apps import custom_app_context as pwd_context
 Base = declarative_base()
 
 #ADD YOUR USER MODEL HERE
+class User(Base):
+	__tablename__ = 'user'
+	id = Column(Integer, primary_key = True)
+	username = Column(String(32), index = True)
+	password_hash = Column(String)
+
+	def hash_password(self, password):
+		self.password_hash = pwd_context.encrypt(password)
+
+	def verify_password(self, password):
+		return pwd_context.verify(password, self.password_hash)
+
 
 class Bagel(Base):
 	__tablename__ = 'bagel'
@@ -23,7 +35,6 @@ class Bagel(Base):
 	    'description' : self.description,
 	    'price' : self.price
 	        }
-
 
 engine = create_engine('sqlite:///bagelShop.db')
  
